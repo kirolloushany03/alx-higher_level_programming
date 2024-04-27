9572ae7e64c215af403847d55f37636e29b78cad
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-27 03:58:11 +0300

    new update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-"""script for finding peak in list of ints, interview prep
-"""
+"""finds a peak in a list of unsorted integers"""
 
-"""
-    lets go to the process
-        it is not sorted, so sorting would take n(log(n))
-            -> not worth sorting
-        looping through and keeping track of max (brute force)
-            -> O(n)
-
-        possibly looping from each end reducing to 1/2 run time
-            -> still O(n)
-"""
 
 def find_peak(lst):
     """Function that finds a peak in a list of unsorted integers"""
