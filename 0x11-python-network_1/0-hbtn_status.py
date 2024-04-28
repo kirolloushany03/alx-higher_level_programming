4ca1ce1fc99a3472d941779d05858bd418cd7979
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-28 21:22:16 +0300

    new update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    this a python script that fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == '__main__':
+    import urllib.request
+    response = urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    read_data = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(read_data)}")
+    print(f"\t- content: {read_data}")
+    print(f"\t- utf8 content: {read_data.decode('utf-8')}")
