1397df8f07afb4db2af3cbc43fd9b8d6b628adc6
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-28 21:23:17 +0300

    new update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 """
 if __name__ == '__main__':
     import urllib.request
-    response = urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    read_data = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(read_data)}")
-    print(f"\t- content: {read_data}")
-    print(f"\t- utf8 content: {read_data.decode('utf-8')}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        read_data = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(read_data)}")
+        print(f"\t- content: {read_data}")
+        print(f"\t- utf8 content: {read_data.decode('utf-8')}")
