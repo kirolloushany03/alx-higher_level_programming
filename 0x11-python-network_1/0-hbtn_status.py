ff9333ea7dc9df28bcf0f1d92389aecef9bae442
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-28 21:51:26 +0300

    new update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
     this a python script that fetches https://alx-intranet.hbtn.io/status
+    and get a response from it
 """
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        read_data = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(read_data)}")
-        print(f"\t- content: {read_data}")
-        print(f"\t- utf8 content: {read_data.decode('utf-8')}")
+    # with urllib.request.urlopen("https://alx-intranet.hbtn.io/status"):
+    response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
+    read_data = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(read_data)}")
+    print(f"\t- content: {read_data}")
+    print(f"\t- utf8 content: {read_data.decode('utf-8')}")
