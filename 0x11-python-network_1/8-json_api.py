25f7ce85bac6974236e5ab4e32f19f659b6780bb
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-29 04:37:15 +0300

    new update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,24 +9,32 @@ import sys
 import json
 
 if __name__ == "__main__":
-    
+    # Default value for q if no argument is given
+    q = ""
+
+    # Check if an argument is given
     if len(sys.argv) > 1:
         q = sys.argv[1]
-    else:
-        q = ""
 
+    # URL to send the POST request to
     url = "http://0.0.0.0:5000/search_user"
 
-    response = requests.post(url, data={"q": q})
+    # Prepare the data to be sent in the POST request
+    data = {'q': q}
 
+    # Send the POST request
+    response = requests.post(url, data=data)
+
+    # Try to parse the response body as JSON
     try:
         response_json = response.json()
 
-        if response.json():
+        # Check if the JSON is not empty
+        if response_json:
+            # Display the id and name for each user in the JSON
             for user in response_json:
-                print(f"[{response.get('id')}] {response.get('name')}")
+                print(f"[{user['id']}] {user['name']}")
         else:
             print("No result")
-
-    except ValueError:
+    except json.JSONDecodeError:
         print("Not a valid JSON")
