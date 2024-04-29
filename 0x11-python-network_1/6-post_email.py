013d7af20789f14029f02b51f9406a8abb8fa179
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-29 04:18:48 +0300

    new update

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+python script that takes in a url and email and sends a post request
+and passed url with the email and displays the body of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+
+    print(response)
