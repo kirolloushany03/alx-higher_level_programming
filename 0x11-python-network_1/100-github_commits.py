74881da2368b2e6e6de86a0186f661670c44b6a6
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-29 04:52:09 +0300

    new update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
     res = requests.get(url)
     commits = res.json()
