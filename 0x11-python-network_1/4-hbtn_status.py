a147be65afc6a29568d287be20bafa5d884af038
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-29 04:05:05 +0300

    new update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,12 +3,12 @@
 python script that fetches website using the requests package
 """
 
-if __name__ == "__main__":
-    import requests
 
-    response = requests.get("https://alx-intranet.hbtn.io/status")
+import requests
 
-    content = response.text
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
+response = requests.get("https://alx-intranet.hbtn.io/status")
+
+content = response.text
+print("Body response:")
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")
