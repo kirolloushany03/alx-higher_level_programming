00f2d0fd921cdd4b1e71ac2422b70edd6645b8fd
root <root@DESKTOP-7O5VQU7.localdomain>
2024-04-29 04:37:52 +0300

    new update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,37 +4,19 @@ python script that takes a letter, sends a post request to url
 with the letter as parameter , displays respns based on json format and content
 """
 
-import requests
-import sys
-import json
-
 if __name__ == "__main__":
-    # Default value for q if no argument is given
-    q = ""
-
-    # Check if an argument is given
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-
-    # URL to send the POST request to
+    import requests
+    import sys
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     url = "http://0.0.0.0:5000/search_user"
 
-    # Prepare the data to be sent in the POST request
-    data = {'q': q}
-
-    # Send the POST request
-    response = requests.post(url, data=data)
-
-    # Try to parse the response body as JSON
+    res = requests.post(url, data={"q": q})
     try:
-        response_json = response.json()
-
-        # Check if the JSON is not empty
-        if response_json:
-            # Display the id and name for each user in the JSON
-            for user in response_json:
-                print(f"[{user['id']}] {user['name']}")
+        resp = res.json()
+        if resp:
+            print(f"[{resp.get('id')}] {resp.get('name')}")
         else:
             print("No result")
-    except json.JSONDecodeError:
+
+    except ValueError:
         print("Not a valid JSON")
